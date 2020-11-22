a02e0f389c0304e2ecb8be602b9a68aba1fe9a0e
Jon <jonathan.morgan.computer@gmail.com>
2020-11-22 16:04:39 -0700

    Fixed the wonkey rectangle and made it so you can click at multiple position
    Also added characters printing to the screen.

--- Sudoku/sudoku.py
+++ Sudoku/sudoku.py
@@ -2,12 +2,13 @@ import sys
 import pygame as pygame
 import sudokuSolver
 
-screenWidth = 601
-screenHeight = 601
+screenWidth = 630 
+screenHeight = 630
 pygame.init()
+pygame.font.init()
+font = pygame.font.SysFont('calibri', int(screenWidth/10))
 
 def drawSudokuScreen(screen):
-    
     screen.fill((255,255,255))
     for i in range(10):
         pygame.draw.line(screen,(128,128,128),(0,i * round((screenWidth/9))), (screenHeight,i * round((screenWidth/9))), 1)
@@ -17,35 +18,65 @@ def drawSudokuScreen(screen):
         pygame.draw.line(screen,(0,0,0),(0,i * round((screenWidth/3))), (screenHeight,i * round((screenWidth/3))), 2)
     for i in range(4):
         pygame.draw.line(screen,(0,0,0),((i * round((screenWidth/3)),0)), (i * round((screenWidth/3)),screenHeight), 2)    
-    pygame.display.flip()
- 
+
+def drawRect(screen,xpos, ypos):
+    if (xpos == None or ypos == None):
+        return
+    else:
+        x = (xpos * (screenWidth/9)+1)
+        y = (ypos * (screenHeight/9)+1)
+
+        pygame.draw.rect(screen, (233,233,233), (x,y,((screenWidth/9) - 1),((screenHeight/9)-1)))
+
+newGrid = [ [1,2,3,  4,5,6,  7,8,9],
+		    [0,0,0,  0,0,0,  0,0,0],
+			[0,0,0,  0,0,0,  0,0,0],
+
+			[0,0,0,  0,0,0,  0,0,0],
+		    [0,0,0,  0,0,0,  0,0,0],
+		    [0,0,0,  0,0,0,  0,0,0],
+
+     	    [0,0,0,  0,0,0,  0,0,0], 
+		    [0,0,0,  0,0,0,  0,0,0],
+		    [0,0,0,  0,0,0,  0,0,0]]
+
+def drawNums(screen):
+    for i in range(9):
+        for j in range(9):
+            if newGrid[i][j] == 0:
+                continue
+            x = font.render(str(newGrid[i][j]),False,(0,0,0))
+            screen.blit(x, ((j * screenWidth/9 + 20), (i * screenHeight/9 + 10)))
+
 
 def main():
     screen = pygame.display.set_mode((screenHeight,screenWidth))
     inputbox = pygame.Rect(0,0,screenWidth/9,screenHeight/9)
-    ibActive = False
-    color = (200,200,200)
-    ibcolorActive = (100,100,100)
-    ibcolorInactive = (200,200,200)
     start = True
+    xpos = None
+    ypos = None
+    
 
     while(start == True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if inputbox.collidepoint(event.pos):
-                    ibActive = True
-                    color = ibcolorActive
-                else:
-                    ibActive = False
-                    color = ibcolorInactive
-                    
+                x,y = pygame.mouse.get_pos()
+                xpos = int(x/(screenWidth/9))
+                ypos = int(y/(screenHeight/9))
+            elif event.type == pygame.KEYDOWN():
+                
         drawSudokuScreen(screen)
-        pygame.draw.rect(screen, color, inputbox, 2)
+        drawRect(screen,xpos, ypos)
+        drawNums(screen)
         pygame.display.flip()
 
 
+
+
 main()
+
+
 
 
 
